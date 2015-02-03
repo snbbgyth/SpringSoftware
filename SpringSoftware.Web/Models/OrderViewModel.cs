4292bcf040ddddbfde026aa373c0f1734eeb23fa
Spring Yang <snbbdx@sina.com>
2015-02-03 17:59:55 +0800

    add modify
    add modify

---
+++ SpringSoftware.Web/Models/OrderViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SpringSoftware.Core.DbModel;
+
+namespace SpringSoftware.Web.Models
+{
+    public class OrderViewModel
+    {
+        public OrderViewModel()
+        {
+            Order=new Order();
+            OrderItemViewList = new List<OrderItemViewModel>();
+        }
+
+        public Order Order { get; set; }
+
+        public IList<OrderItemViewModel> OrderItemViewList { get; set; }
+    }
+}
